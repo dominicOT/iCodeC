a9b2adbb40426005a4fe5126ac8f1027717110d8
Dominic Oladapo-Tonade <110516773+dominicOT@users.noreply.github.com>
2023-05-17 04:24:47 +0100

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,21 @@
+/**
+File Name: fibonnaci series.c
+Author: Dominic OT
+Date: 16-05-2023
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    int d, k, sum;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &d, &k);
+    
+	sum = d + k;
+    
+	printf("Sum = %d", sum);
+    
+	return 0;
+}
---
+++ even or odd.c
@@ -0,0 +1,26 @@
+/**
+File Name: fibonnaci series.c
+Author: Dominic OT
+Date: 16-05-2023
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+	if(num % 2 == 0) 
+	{
+	    printf("%d is even", num);
+	}
+    else
+    {
+        printf("%d is odd", num);
+	}
+    return 0;
+}
+
